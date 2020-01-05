e29b1a7e61ef164f5f7fd87b6203ad086a8f734b
Szymon <szymon.nedzi@gmail.com>
2020-01-05 21:36:00 +0100

    move data and methods from GameFragment to GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -4,14 +4,58 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 
-class GameViewModel : ViewModel(){
+class GameViewModel : ViewModel() {
+
+    var word = ""
+
+    // The current score
+    var score = 0
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen", "hospital", "basketball", "cat", "change", "snail", "soup", "calendar",
+                "sad", "desk", "guitar", "home", "railway", "zebra", "jelly", "car",
+                "crow", "trade", "bag", "roll", "bubble")
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //         gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+
     init {
-        Log.i("GameViewModel","GameViewModel created'")
+        Log.i("GameViewModel", "GameViewModel created'")
+        resetList()
+        nextWord()
+
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel","GameViewModel cleared'")
+        Log.i("GameViewModel", "GameViewModel cleared'")
 
     }
 }
